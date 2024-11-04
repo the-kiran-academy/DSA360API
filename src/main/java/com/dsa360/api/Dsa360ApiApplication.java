6a1b94d8b5d4dc3b5fc5c4784f536d4d5ef6375c
fali-sah <fali.sah90@gmail.com>
2024-11-04 11:08:30 +0530

    task 1

--- src/main/java/com/dsa360/api/Dsa360ApiApplication.java
+++ src/main/java/com/dsa360/api/Dsa360ApiApplication.java
@@ -8,6 +8,8 @@ public class Dsa360ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dsa360ApiApplication.class, args);
+		
+		System.err.println("application changes");
 	}
 
 }
