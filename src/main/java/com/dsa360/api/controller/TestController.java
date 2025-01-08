f56bdf71c1d4bc2ab4b284211370801eefe23c11
Omkar-Patil <patilomkar7624@gmail.com>
2025-01-08 20:55:29 +0530

    Omkar_task1

---
+++ src/main/java/com/dsa360/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.dsa360.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	@GetMapping("/push")
+	public String testing() {
+		return "Push on Git";
+	}
+}
